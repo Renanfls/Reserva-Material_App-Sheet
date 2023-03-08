ad5a599ad2fc34a10e9fce0f741670a178e5d1c2
Renanfls <contato.renanfabricio@gmail.com>
2023-03-08 17:07:52 -0300

    registro na ultima linha da baseDeDados

--- js/Código.js
+++ js/Código.js
@@ -1,14 +1,17 @@
 function myFunction(nome, photo, address, useremail, registro, id) {
   // Refere ao arquivo
-  var ss = SpreadsheetApp.openByUrl("https://docs.google.com/spreadsheets/d/18xhENLF2t5PbBifMQRKtlvFQ8xnz7Xd5bZ-LIP2pa-U/edit#gid=27715399")
+  var ss = SpreadsheetApp.openByUrl("https://docs.google.com/spreadsheets/d/18xhENLF2t5PbBifMQRKtlvFQ8xnz7Xd5bZ-LIP2pa-U/edit#gid=27715399");
   // Refere a aba
-  var registroAnteriores = ss.getSheetByName("registroAnteriores")
+  var registroAnteriores = ss.getSheetByName("registroAnteriores");
+
+  var ultimaLinha = registroAnteriores.getLastRow() + 1;
 
   // getRange(2, 1) isso quer dizer célula A2
-  registroAnteriores.getRange(2, 1).setValue(id)
-  registroAnteriores.getRange(2, 3).setValue(nome)
-  registroAnteriores.getRange(2, 4).setValue(photo)
-  registroAnteriores.getRange(2, 5).setValue(address)
-  registroAnteriores.getRange(2, 6).setValue(useremail)
-  registroAnteriores.getRange(2, 7).setValue(registro)
+  registroAnteriores.getRange(ultimaLinha, 1).setValue(id);
+  registroAnteriores.getRange(ultimaLinha, 2).setValue(idvisitante);
+  registroAnteriores.getRange(ultimaLinha, 3).setValue(nome);
+  registroAnteriores.getRange(ultimaLinha, 4).setValue(photo);
+  registroAnteriores.getRange(ultimaLinha, 5).setValue(address);
+  registroAnteriores.getRange(ultimaLinha, 6).setValue(useremail);
+  registroAnteriores.getRange(ultimaLinha, 7).setValue(registro);
 }
